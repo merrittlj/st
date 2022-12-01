af24b859dc193c0459c8e9876dd245702a9c4dee
Lucas Merritt <lucas.j.merritt@gmail.com>
2022-12-01 11:45:36 -0700

    light theme baby

--- config.h
+++ config.h
@@ -94,41 +94,31 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-   /* 8 normal colors */
-   "black",
-   "red3",
-   "green3",
-   "yellow3",
-   "blue2",
-   "magenta3",
-   "cyan3",
-   "gray90",
-
-   /* 8 bright colors */
-   "gray50",
-   "red",
-   "green",
-   "yellow",
-   "#5c5cff",
-   "magenta",
-   "cyan",
-   "white",
-
-   [255] = 0,
-
-   /* more colors can be added after 255 to use with DefaultXX */
-   "#cccccc",
-   "#555555",
-   "gray90", /* default foreground colour */
-   "black", /* default background colour */
+    "#fbf1c7", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
+    "#cc241d",
+    "#98971a",
+    "#d79921",
+    "#458588",
+    "#b16286",
+    "#689d6a",
+    "#7c6f64",
+    "#928374",
+    "#9d0006",
+    "#79740e",
+    "#b57614",
+    "#076678",
+    "#8f3f71",
+    "#427b58",
+    "#3c3836",
 };
+
 /*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+* Default colors (colorname index)
+* foreground, background, cursor
+*/
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 /*
  * Default shape of cursor
